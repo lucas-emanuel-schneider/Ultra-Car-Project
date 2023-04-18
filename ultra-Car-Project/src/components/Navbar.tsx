9e8b5d073cd71af0f524a0846854ead48ee610bb
Lucas <lucas_emanuel_sc@hotmail.com>
2023-04-17 22:24:02 -0300

    feat: nav bar and login

--- ultra-Car-Project/src/components/Navbar.tsx
+++ ultra-Car-Project/src/components/Navbar.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate, NavLink } from 'react-router-dom'
+import logo from '../images/cabecalho.png'
+
 
 function Navbar() {
+  const [currentUser, setCurrentUser] = useState('');
+  const navigate = useNavigate();
+  useEffect(() => {
+    const userStorageData = localStorage.getItem('user_db');
+    if (userStorageData) setCurrentUser(JSON.parse(userStorageData).email)
+  }, []);
+
+  const handleClickOnLogo = () => navigate('/home');
+  const logout = () => {
+    localStorage.removeItem('fake_token');
+    localStorage.removeItem('user_db');
+    localStorage.removeItem('new_jobs');
+    navigate('/login')
+  }
   return (
-    <div>Navbar</div>
+    <nav>
+      <div>
+        <img src={ logo } alt="UltraCar" onClick={handleClickOnLogo} />
+      </div>
+      <ul>
+          <li>
+            {currentUser}
+          </li>
+        <NavLink to="/clients">
+          <li>
+            Clientes
+          </li>
+        </NavLink>
+        <NavLink to="/stock">
+          <li>
+            Estoque
+          </li>
+        </NavLink>
+      </ul>
+      <button onClick={logout}>Logout</button>
+    </nav>
   )
 }
 
